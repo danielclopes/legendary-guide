3e9bbb2eaa072876257240d540aaa083c9ae5986
12111254 <12111254@l1817972.portoalegre.pucrsnet.br>
2017-05-15 20:01:24 -0300

    Primeiro teste com Heaps

---
+++ bst-2017-1/src/br/pucrs/ap3/heaps/Heap.java
@@ -0,0 +1,10 @@
+package br.pucrs.ap3.heaps;
+
+public class Heap {
+
+	public static boolean isMaxHeap(int[] v) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ bst-2017-1/src/br/pucrs/ap3/heaps/HeapTest.java
@@ -0,0 +1,19 @@
+package br.pucrs.ap3.heaps;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HeapTest {
+
+	@Test
+	public void testNotMaxHeap() {
+		int[] v = {15, 60, 72, 70, 56, 32, 62, 92, 45, 30, 65};
+		// 15 60 72 70 56 32 62 92 45 30 65
+		// false
+		boolean expected = false;
+		boolean actual = Heap.isMaxHeap(v);
+		assertEquals(expected, actual);
+	}
+
+}
